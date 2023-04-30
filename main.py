c898f20424fc929acbaa098a5be97d6fb3f139ff
doolet <daulet.medeubayev@gmail.com>
2023-05-01 05:59:34 +0600

    Changed data type to datetime

--- main.py
+++ main.py
@@ -5,3 +5,14 @@ import pandas as pd
 df = pd.read_csv(r'./data/uber_data.csv')
 df.head()
 # %%
+df.info()
+# %%
+df['tpep_pickup_datetime'] = pd.to_datetime(df['tpep_pickup_datetime'])
+df['tpep_dropoff_datetime'] = pd.to_datetime(df['tpep_dropoff_datetime'])
+# %%
+df.info()
+# %%
+df = df.drop_duplicates().reset_index(drop=True)
+df['trip_id'] = df.index
+# %%
+df.head()
